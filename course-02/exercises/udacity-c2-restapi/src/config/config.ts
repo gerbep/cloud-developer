04d6039122983e671c9eb4a06772e6ee35b339b1
Gerray <g.fellisch@googlemail.com>
2020-07-11 12:25:38 +0200

    L4: GET by id, PATCH by id

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   "dev": {
-    "username": "udagramfelldev",
-    "password": "udagramfelldev",
-    "database": "udagramfelldev",
-    "host": "udagramfelldev.crk9xvohinik.eu-central-1.rds.amazonaws.com",
+    "username": "udagramgfell",
+    "password": "udagramgfell",
+    "database": "udagramgfell",
+    "host": "udagramgfell.crk9xvohinik.eu-central-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "eu-central-1b",
+    "aws_region": "eu-central-1",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-fellisch-dev"
   },
